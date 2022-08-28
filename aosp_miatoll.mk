e722bf2eb69c08ac9562b2d629658c630a8c2b6e
vega64 <nyjahuston58@gmail.com>
2022-08-28 14:07:54 +0000

    miatoll: Inherit common.mk from our vendor

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
