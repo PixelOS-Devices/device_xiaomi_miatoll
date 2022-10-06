fa06b686ff62f757e13ebcd9267ca91a431e13fc
Zinadin Zidan <zidan44@pixelexperience.org>
2022-10-06 11:32:43 +0600

    sm6250-common: Call vibrator-vendor-product.mk
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I8243285efaf1161616eabb1fa4b916237a8ca67a

--- common.mk
+++ common.mk
@@ -430,11 +430,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # VNDK
 $(foreach target, $(shell cat $(LOCAL_PATH)/vndk.txt), $(eval PRODUCT_PACKAGES += $(target).vendor))
