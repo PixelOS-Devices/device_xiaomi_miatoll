b10d3eeababc33163cf0c52e3e3a852a0280992e
Avanindra Ray <rayavanindra@gmail.com>
2022-10-17 19:38:52 +0530

    sm6250-common: Add back component-override
    Signed-off-by: COSMIC <rayavanindra@gmail.com>

--- common.mk
+++ common.mk
@@ -106,6 +106,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Component override
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+
 # Device Settings
 PRODUCT_PACKAGES += \
     XiaomiParts
