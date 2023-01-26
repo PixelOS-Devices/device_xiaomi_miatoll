5dcd60169422d0952e1f955f005960c663a7d869
Mathieu Chartier <mathieuc@google.com>
2023-01-26 17:43:42 +0800

    sm6250-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- common.mk
+++ common.mk
@@ -363,6 +363,10 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Shim
 PRODUCT_PACKAGES += \
     libwatermark_shim
