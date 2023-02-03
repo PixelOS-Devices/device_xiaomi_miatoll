3fb3b8fad063b9e6351286264589081255422cca
Nauval Rizky <enuma.alrizky@gmail.com>
2023-02-04 01:07:39 +0800

    sm6250-common: Move msm_irqbalance config to device tree
    Change-Id: I5210e90a06f8af6706cd222b703c1ff7f7acc3f5
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- common.mk
+++ common.mk
@@ -253,6 +253,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fakelogprint
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
