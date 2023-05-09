0dbf279bcdfbcef0d6569500d72e1c46218010c3
Alexander Winkowski <dereference23@outlook.com>
2023-05-09 15:42:25 +0000

    sm6250-common: Build Xiaomi sensors Multi-HAL
    This allows to use Xiaomi pickup sensor.
    
    Change-Id: I96425bc0907b37ebebfca8600eb07c71f3afaab8

--- common.mk
+++ common.mk
@@ -380,7 +380,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
 # Soong namespaces
