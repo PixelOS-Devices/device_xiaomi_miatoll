87937a8f956b327c7c141f2fc315430c5faba811
COSMIC <rayavanindra@gmail.com>
2023-06-05 08:34:07 +0530

    Revert "sm6250-common: Add the missing vendor.qti.imsrtpservice@3.1"
    This reverts commit 8f058c18c83da36b1987c5c8991234e7198e9799.
    
    *Also fix extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -70,7 +70,7 @@ extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
 if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
     extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
             "${KANG}" --section "${SECTION}"
