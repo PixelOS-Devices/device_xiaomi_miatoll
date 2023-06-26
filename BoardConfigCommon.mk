d8827be554c87c503b6401fe37ac0bcc31da9c15
COSMIC <rayavanindra@gmail.com>
2023-06-26 15:13:02 +0530

    sm6250-common: Set neverallows flag to false for user build

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -205,7 +205,7 @@ include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
+SELINUX_IGNORE_NEVERALLOWS := false
 
 # Treble
 BOARD_VNDK_VERSION := current
