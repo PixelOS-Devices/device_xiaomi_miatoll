4f51954fac5724af6cff7a2d68e33801d9c5a202
COSMIC <rayavanindra@gmail.com>
2023-09-24 22:49:48 +0530

    Revert "sm6250-common: Don't inherit updatable_apex.mk"
    This reverts commit b7c8520a243e52dc10cdd71ff9154399fa57d6ce.

--- common.mk
+++ common.mk
@@ -7,6 +7,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Apex
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit proprietary targets
 $(call inherit-product-if-exists, vendor/xiaomi/sm6250-common/sm6250-common-vendor.mk)
 
